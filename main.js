61c6a47e070569b81b57c7af26e1ac91af4f01a7
fecordeiro613 <fcordeiro613@gmail.com>
2025-03-20 21:10:02 -0300

    Classe com dados e booleano

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-function Pessoa (a,b){    //uma função que é uma classe 
-    this.nome = a;
-    this.idade = b;     //atributos da classe definidos
-}
+function Carro (a,b,c,d,e){    //uma função que é uma classe 
+    this.modelo = a;
+    this.cor = b;
+    this.ano = c;
+    this.valor = d;
+    this.popular = e;
 
-const p = new Pessoa ('Afonso', 31);  //declarou a var p classe 
+
+    if (e = true){
+        console.log("o carro é popular ");
+    }else{
+        console.log ("o número não é popular");
+    }
+}    
 
 
 
-console.log (p);
+
+
+var carro = new Carro ('siena', 'laranja', 2006, 80000  , true);  //declarou a var p classe 
+
+
+console.log (carro);
 
 //praticamente POO com Python do 3 semestre mas com JS
